7a4324e93a4fd98fc46e9e838776f913e5a5a630
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-15 22:45:17 +0800

    class Bit

---
+++ include/jccpp/Bit.h
@@ -0,0 +1,38 @@
+#pragma once
+#include<bit>
+#include<stdint.h>
+
+namespace jccpp
+{
+	class Bit
+	{
+	private:
+		Bit() = delete;
+		Bit(Bit const &o) = delete;
+		Bit(Bit const &&o) = delete;
+		Bit &operator=(Bit const &o) = delete;
+
+	public:
+		/// <summary>
+		///		从最高位开始数，有多少个连续的 0.
+		/// </summary>
+		/// <param name="num"></param>
+		/// <returns></returns>
+		static constexpr int HighZeroCount(uint32_t num)
+		{
+			return std::countl_zero(num);
+		}
+
+		/// <summary>
+		///		最高位的 1 的索引。
+		///		* 例如 0x1 的 bit0 是最高位的 1，于是返回 0.
+		/// </summary>
+		/// <param name="num"></param>
+		/// <returns></returns>
+		static constexpr int HighestOneBitIndex(uint32_t num)
+		{
+			int count = HighZeroCount(num);
+			return 32 - count - 1;
+		}
+	};
+}
