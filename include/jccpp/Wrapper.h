073173c1a883ed438465ee6bee7627420df6fb1d
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-05-26 00:07:42 +0800

    class Wrapper

--- include/jccpp/Wrapper.h
+++ include/jccpp/Wrapper.h
@@ -1,11 +1,5 @@
 #pragma once
-#include<iostream>
-using std::cout;
-using std::endl;
-#include<functional>
-using std::function;
 #include<memory>
-using std::shared_ptr;
 
 template <class T>
 class Wrapper
